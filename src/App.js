049e18db50c83c58fbdb581d9abdd8b932740be7
JonathanChen <jonny29009@yahoo.com>
2020-05-02 16:36:14 -0700

    refactor code

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import './App.css';
+import MazeSolver from './components/MazeSolver';
 
 function App() {
   return (
     <div>
-      <h1>Hello World</h1>
+      <MazeSolver />
     </div>
   );
 }
---
+++ src/components/MazeSolver/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const MazeSolver = () => {
+    return (
+        <h1>I am a maze solver</h1>
+    )
+}
+
+export default MazeSolver;
