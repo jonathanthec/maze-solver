141cf1c0ea4de89a708b1a6e16b1dd2af85ad882
JonathanChen <jonny29009@yahoo.com>
2020-05-06 00:07:47 -0700

    add footer bar

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,27 @@
 import React from 'react';
+import { FaGithub, FaLinkedinIn } from 'react-icons/fa';
 import "./styles/Footer.css";
 
 export const Footer = () => {
     return (
-        <div className="footer-container">
-            <h6>Created by Jonathan Chen</h6>
-            <p><a href="www.google.com" target="__blank">Check out my other projects!</a></p>
-        </div>
+        <footer>
+            <div className="footer-container">
+                <ul className="footer-buttons">
+                    <li>
+                        <a id="github" href="https://github.com/jonathanthec" target="_blank" rel="noopener noreferrer">
+                            <FaGithub style={{ fontSize: "1.6rem" }} />
+                        </a>
+                    </li>
+                    <li>
+                        <a id="linkedin" href="https://www.linkedin.com/in/jonathan-chen-86589686/" target="_blank" rel="noopener noreferrer">
+                            <FaLinkedinIn style={{ fontSize: "1.6rem" }} />
+                        </a>
+                    </li>
+                </ul>
+                <div className="footer-copyright">
+                    <p><i className="fa fa-copyright"></i> Jonathan Chen 2020</p>
+                </div>
+            </div>
+        </footer>
     )
 }
